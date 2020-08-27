06d6eb9b01526cb005165d3ed95afea266057319
Evan <3van111@gmail.com>
2020-08-27 16:57:46 +0700

    Follow system and unit test

--- config.py
+++ config.py
@@ -7,3 +7,9 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     START_NGROK = os.environ.get('START_NGROK') is not None and \
         os.environ.get('WEKZEUG_RUN_MAIN') != 'true'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['3van111@gmail.com']

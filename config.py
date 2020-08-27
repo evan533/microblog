b59f7d04fc17d975027a0ef294e20bc3fdc0ec0f
Evan <3van111@gmail.com>
2020-08-27 14:36:30 +0700

    Add login and profile system

--- config.py
+++ config.py
@@ -5,3 +5,5 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    START_NGROK = os.environ.get('START_NGROK') is not None and \
+        os.environ.get('WEKZEUG_RUN_MAIN') != 'true'
--- microblog.py
+++ microblog.py
@@ -4,3 +4,12 @@ from app.models import User,Post
 @app.shell_context_processor
 def make_shell_context():
     return{'db':db, 'User':User, 'Post':Post}
+
+def start_ngrok():
+    from pyngrok import ngrok
+
+    url = ngrok.connect(5000)
+    print('* Tunnel URL', url)
+
+if app.config['START_NGROK']:
+    start_ngrok()
